70154088fa74eeec74a1dd3ba50c73e6cc0d2803
Vinayak Agarwal <vinayakagarwal6996@gmail.com>
2016-04-27 08:50:12 +0530

    New User

--- FyndDyne/AdminPortal.xaml.cs
+++ FyndDyne/AdminPortal.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +32,32 @@ namespace FyndDyne
             MainWindow.Type = null;
             new MainWindow().Show();
             this.Close();
+        }
+
+        private void SubmitButtonAction(object sender, RoutedEventArgs e)
+        {
+            string query = "INSERT INTO Manager values('" + UserName.Text + "', '"+ Utilities.MD5(Password.Password) +
+                "', '" + FirstName.Text + "', '" + LastName.Text + "');" ;
+            string query2 = "INSERT INTO Restaurant values( 1,'" + UserName.Text + "', '" + Name.Text +
+                "','" + Phone.Text + "','" + StreetAddress.Text + "','" + City.Text + "','" + State.Text +
+                "','" + ZipCode.Text + "', '');";
+            try {
+                var dbCon = DBConnection.Instance();
+                dbCon.Open();
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                var reader = cmd.ExecuteReader();
+                cmd = new MySqlCommand(query2, dbCon.Connection);
+                reader = cmd.ExecuteReader();
+                reader.Close();
+                dbCon.Close();
+                this.Close();
+                new AdminPortal().Show();                  
+            }
+            catch(MySqlException ex) {
+                Trace.WriteLine(ex);
+            }
+            Trace.WriteLine(query);
+            Trace.WriteLine(query2);
         }
     }
 }
