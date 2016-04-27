cf3d481eab87792afc24455f429525b9459e7c1e
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-04-27 18:14:54 +0530

    Final Commit

--- FyndDyne/Cart.xaml.cs
+++ FyndDyne/Cart.xaml.cs
@@ -110,10 +110,10 @@ namespace FyndDyne {
                 string query = String.Format("SELECT SUM(qty*price) FROM cart NATURAL JOIN product"); 
                 var cmd = new MySqlCommand(query, dbCon.Connection);
                 string total = ((decimal) cmd.ExecuteScalar()).ToString();
-                query = String.Format("INSERT INTO Orders(u_id, fde_id, total_cost, delivery_type, status) VALUES ('{0}', null, {1}, '{2}', '{3}) WHERE u_id='{0}'", MainWindow.User, total, deltype, status);
+                query = String.Format("INSERT INTO Orders(u_id, fde_id, total_cost, delivery_type, status) VALUES ('{0}', null, {1}, '{2}', '{3}')", MainWindow.User, total, deltype, status);
                 cmd = new MySqlCommand(query, dbCon.Connection);
                 cmd.ExecuteNonQuery();
-                query = String.Format("SELECT o_id FROM Order WHERE u_id = '{0}' ORDER BY o_id DESC", MainWindow.User);
+                query = String.Format("SELECT o_id FROM Orders WHERE u_id = '{0}' ORDER BY o_id DESC", MainWindow.User);
                 cmd = new MySqlCommand(query, dbCon.Connection);
                 var reader = cmd.ExecuteReader();
                 reader.Read();
@@ -122,17 +122,27 @@ namespace FyndDyne {
                 query = String.Format("SELECT * from CART WHERE u_id = '{0}'", MainWindow.User);
                 cmd = new MySqlCommand(query, dbCon.Connection);
                 reader = cmd.ExecuteReader();
+                int n = 0;
+                string[] p_ids = new string[50];
+                string[] qts = new string[50];
                 while(reader.Read()) {
-                   var q = String.Format("INSERT INTO Order_Product VALUES({0}, {1}, {2})", o_id, reader.GetString("p_id"), reader.GetString("qty"));
+                    
+                    p_ids[n] = reader.GetString("p_id");
+                    qts[n] = reader.GetString("qty");
+                    n++;
+                }
+                reader.Close();
+                for (int i=0; i< n; i++) {
+                    var q = String.Format("INSERT INTO Order_Product VALUES({0}, {1}, {2})", o_id, p_ids[i], qts[i]);
                     var cmd2 = new MySqlCommand(q, dbCon.Connection);
                     cmd2.ExecuteNonQuery();
                 }
-                reader.Close();
+               
                 query = String.Format("DELETE from CART WHERE u_id = '{0}'", MainWindow.User);
                 cmd = new MySqlCommand(query, dbCon.Connection);
                 cmd.ExecuteNonQuery();
                 dbCon.Close();
-                MessageBox.Show("Ordered! Your total will be: " + total);
+                MessageBox.Show("Ordered! Your total will be: " + total, "Sucess");
                 new MainWindow().Show();
                 this.Close();
             }
